655624b694738a88fff1cb6e0f9b72c18045d300
Ishan Singh <ishsin.2020@gmail.com>
2024-03-13 23:33:33 +0530

    merging again

--- env/Library/bin/c_rehash.pl
+++ env/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/Users/crucial/Desktop/Deep-NLP-A-Z/env\\Library";
+my $prefix = "C:/chatbot/env\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
